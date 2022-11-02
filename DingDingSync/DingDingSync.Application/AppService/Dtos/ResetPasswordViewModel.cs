8a588837fa94989dd07db0068f7b2b3210b774a8
郑伟 <511927343@qq.com>
2022-11-02 21:12:41 +0800

    修改密码强度

--- DingDingSync/DingDingSync.Application/AppService/Dtos/ResetPasswordViewModel.cs
+++ DingDingSync/DingDingSync.Application/AppService/Dtos/ResetPasswordViewModel.cs
@@ -14,6 +14,8 @@ namespace DingDingSync.Application.AppService.Dtos
 
         [MaxLength(16, ErrorMessage = "{0}长度超过限制")]
         [Required(ErrorMessage = "新密码不能为空")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#^*()\-_\.\[\]:])[A-Za-z\d!@#^*()\-_\.\[\]:]{8,16}$",
+            ErrorMessage = "密码必须同时包含大小写英文字母、数字、特殊字符（!@#^*()-_.[]:）")]
         public string NewPassword { get; set; }
 
         [Required(ErrorMessage = "确认密码不能为空")]
