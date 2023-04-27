f6a3c1c205b12e4b623372cdbeaa3fac4554bd6a
郑伟 <511927343@qq.com>
2023-04-27 23:47:27 +0800

    添加爱快功能判断

--- DingDingSync/DingDingSync.Application/Jobs/IKuaiSyncAccountBackgroundJob.cs
+++ DingDingSync/DingDingSync.Application/Jobs/IKuaiSyncAccountBackgroundJob.cs
@@ -7,6 +7,7 @@ using DingDingSync.Application.DingDingUtils;
 using DingDingSync.Application.IKuai;
 using DingDingSync.Application.IKuai.Dtos;
 using DingDingSync.Core.Entities;
+using Microsoft.Extensions.Options;
 
 namespace DingDingSync.Application.Jobs;
 
@@ -16,71 +17,78 @@ public class IKuaiSyncAccountBackgroundJob : BackgroundJob<string>, ITransientDe
     private readonly IkuaiAppService _ikuaiAppService;
     private readonly ILogger _logger;
     private readonly IDingdingAppService _dingdingAppService;
+    private readonly IKuaiConfigOptions _ikuaiConfigOptions;
 
     public IKuaiSyncAccountBackgroundJob(IRepository<UserEntity, string> userRepository,
-        IkuaiAppService ikuaiAppService, ILogger logger, IDingdingAppService dingdingAppService)
+        IkuaiAppService ikuaiAppService, ILogger logger, IDingdingAppService dingdingAppService,
+        IOptions<IKuaiConfigOptions> options)
     {
         _userRepository = userRepository;
         _ikuaiAppService = ikuaiAppService;
         _logger = logger;
         _dingdingAppService = dingdingAppService;
+        _ikuaiConfigOptions = options.Value;
     }
 
-    
+
     [UnitOfWork]
     public override void Execute(string userId)
     {
-        try
+        // 若启用爱快才执行以下步骤
+        if (_ikuaiConfigOptions.IsEnabled)
         {
-            var message = string.Empty;
-            // 根据userId查询用户信息
-            var user = _userRepository.FirstOrDefault(userId);
-            // 若用户不为空 判断VPN是否启用
-            if (user != null && user.VpnAccountEnabled)
+            try
             {
-                var ikuaiAccount = _ikuaiAppService.GetAccountIdByUsername(user.UserName);
-                // 域账号被删除
-                if (user.IsDeleted)
+                var message = string.Empty;
+                // 根据userId查询用户信息
+                var user = _userRepository.FirstOrDefault(userId);
+                // 若用户不为空 判断VPN是否启用
+                if (user != null && user.VpnAccountEnabled)
                 {
-                    // 爱快账号不为空
-                    if (ikuaiAccount != null)
+                    var ikuaiAccount = _ikuaiAppService.GetAccountIdByUsername(user.UserName);
+                    // 域账号被删除
+                    if (user.IsDeleted)
                     {
-                        _ikuaiAppService.RemoveAccount(ikuaiAccount.id);
-                        message = "已将你的VPN账号删除，如有疑问，请联系管理员";
-                    }
-                }
-                else
-                {
-                    // 解密用户密码                                                                                         
-                    var password = user.Password.DesDecrypt();
-                    // 同步用户名、密码至爱快开放平台（判断账号是否创建，未创建调用新建接口，已建，调用修改接口）
-                    if (ikuaiAccount == null)
-                    {
-                        _ikuaiAppService.CreateAccount(new AccountCommon(user.UserName, password, user.Name));
-                        
-                        message = $"已为您开通VPN，账号密码与域账号相同；账号为：{user.UserName} ，密码为：{password} ，请勿泄露！";
+                        // 爱快账号不为空
+                        if (ikuaiAccount != null)
+                        {
+                            _ikuaiAppService.RemoveAccount(ikuaiAccount.id);
+                            message = "已将你的VPN账号删除，如有疑问，请联系管理员";
+                        }
                     }
                     else
                     {
-                        ikuaiAccount.share = 2;
-                        ikuaiAccount.passwd = password;
-                        ikuaiAccount.enabled = "yes";
-                        _ikuaiAppService.UpdateAccount(ikuaiAccount);
-                        
-                        message = $"域账号信息与VPN已同步；账号为：{user.UserName}，密码为：{password} ，请勿泄露！";
+                        // 解密用户域账号密码                                                                                         
+                        var password = user.Password.DesDecrypt();
+                        // 同步用户名、密码至爱快开放平台（判断账号是否创建，未创建调用新建接口，已建，调用修改接口）
+                        if (ikuaiAccount == null)
+                        {
+                            _ikuaiAppService.CreateAccount(new AccountCommon(user.UserName, password, user.Name));
+
+                            message = $"已为您开通VPN，账号密码与域账号相同；账号为：{user.UserName} ，密码为：{password} ，请勿泄露！";
+                        }
+                        else
+                        {
+                            ikuaiAccount.share = 2;
+                            ikuaiAccount.passwd = password;
+                            ikuaiAccount.enabled = "yes";
+                            _ikuaiAppService.UpdateAccount(ikuaiAccount);
+
+                            message = $"域账号信息与VPN已同步；账号为：{user.UserName}，密码为：{password} ，请勿泄露！";
+                        }
                     }
+
+                    _dingdingAppService.SendTextMessage(userId, message);
                 }
-                                        
-                _dingdingAppService.SendTextMessage(userId, message);
             }
-        }
-        catch (IKuaiException e)
-        {
-            _logger.Error("调用iKuai接口时发生异常", e);
-        }
-        catch (Exception e)
-        {
-            _logger.Error("处理VPN账号时发生异常", e);
+            catch (IKuaiException e)
+            {
+                _logger.Error("调用iKuai接口时发生异常", e);
+            }
+            catch (Exception e)
+            {
+                _logger.Error("处理VPN账号时发生异常", e);
+            }
         }
     }
 }
