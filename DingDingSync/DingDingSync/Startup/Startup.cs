7de2a612344b4406f46d24d93bc79dbfd33068b8
郑伟 <511927343@qq.com>
2023-06-16 10:31:09 +0800

    提取扩展方法

--- DingDingSync/DingDingSync/Startup/Startup.cs
+++ DingDingSync/DingDingSync/Startup/Startup.cs
@@ -44,11 +44,7 @@ namespace DingDingSync.Web.Startup
 
             services.Configure<IKuaiConfigOptions>(Configuration.GetSection(IKuaiConfigOptions.IKuai));
 
-            // todo 注释掉，观察是否不需要手工注入
-            // services.AddScoped<IDingdingAppService, DingDingAppService>();
-            // services.AddScoped<IWorkWeixinAppService, WorkWeixinAppService>();
-
-            RegisterDingDingEventHandler(services);
+            services.RegisterDingDingEventHandler();
 
             var workEnv = Configuration["WorkEnv"];
             if (workEnv.Equals("DingDing", StringComparison.OrdinalIgnoreCase))
@@ -70,59 +66,6 @@ namespace DingDingSync.Web.Startup
                 options.IocManager.IocContainer.AddFacility<LoggingFacility>(facility =>
                     facility.UseAbpLog4Net()
                         .WithConfig(Environment.IsDevelopment() ? "log4net.Development.config" : "log4net.config"));
-            });
-        }
-
-        /// <summary>
-        /// 注册钉钉事件处理程序
-        /// </summary>
-        /// <param name="services"></param>
-        private static void RegisterDingDingEventHandler(IServiceCollection services)
-        {
-            services.AddTransient<UserAddOrgEventHandler>(); //通讯录用户增加
-            services.AddTransient<UserModifyOrgEventHandler>(); //通讯录用户更改
-            services.AddTransient<UserLeaveOrgEventHandler>(); //通讯录用户离职
-            services.AddTransient<UserActiveOrgEventHandler>(); //加入企业后用户激活
-            services.AddTransient<OrgAdminAddEventHandler>(); //通讯录用户被设为管理员
-            services.AddTransient<OrgAdminRemoveEventHandler>(); //通讯录用户被取消设置管理员
-            services.AddTransient<OrgDeptCreateEventHandler>(); //通讯录企业部门创建
-            services.AddTransient<OrgDeptModifyEventHandler>(); //通讯录企业部门修改
-            services.AddTransient<OrgDeptRemoveEventHandler>(); //通讯录企业部门删除
-            services.AddTransient<OrgRemoveEventHandler>(); //企业被解散
-            services.AddTransient<LabelUserChangeEventHandler>(); //员工角色信息发生变更
-            services.AddTransient(serviceProvider =>
-            {
-                Func<string, DingdingBaseEventHandler> func = (eventType) =>
-                {
-                    switch (eventType)
-                    {
-                        case "user_add_org": //通讯录用户增加
-                            return serviceProvider.GetService<UserAddOrgEventHandler>();
-                        case "user_modify_org": //通讯录用户更改
-                            return serviceProvider.GetService<UserModifyOrgEventHandler>();
-                        case "user_leave_org": //通讯录用户离职
-                            return serviceProvider.GetService<UserLeaveOrgEventHandler>();
-                        case "user_active_org": //加入企业后用户激活
-                            return serviceProvider.GetService<UserActiveOrgEventHandler>();
-                        case "org_admin_add": //通讯录用户被设为管理员
-                            return serviceProvider.GetService<OrgAdminAddEventHandler>();
-                        case "org_admin_remove": //通讯录用户被取消设置管理员
-                            return serviceProvider.GetService<OrgAdminRemoveEventHandler>();
-                        case "org_dept_create": //通讯录企业部门创建
-                            return serviceProvider.GetService<OrgDeptCreateEventHandler>();
-                        case "org_dept_modify": //通讯录企业部门修改
-                            return serviceProvider.GetService<OrgDeptModifyEventHandler>();
-                        case "org_dept_remove": //通讯录企业部门删除
-                            return serviceProvider.GetService<OrgDeptRemoveEventHandler>();
-                        case "org_remove": //企业被解散
-                            return serviceProvider.GetService<OrgRemoveEventHandler>();
-                        case "label_user_change": //员工角色信息发生变更
-                            return serviceProvider.GetService<LabelUserChangeEventHandler>();
-                        default:
-                            return null;
-                    }
-                };
-                return func;
             });
         }
 
