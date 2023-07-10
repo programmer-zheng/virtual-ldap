56b4eb8dbe983d99ae940cbb478a39316b003761
郑伟 <511927343@qq.com>
2023-07-10 23:23:38 +0800

    钉钉、企业微信配置类添加验证

--- ContactsSync/VirtualLdap.Application/DingDingUtils/DingTalkConfigOptions.cs
+++ ContactsSync/VirtualLdap.Application/DingDingUtils/DingTalkConfigOptions.cs
@@ -1,4 +1,6 @@
-﻿namespace VirtualLdap.Application.DingDingUtils
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VirtualLdap.Application.DingDingUtils
 {
     public class DingTalkConfigOptions
     {
@@ -7,31 +9,38 @@
         /// <summary>
         /// CorpId
         /// </summary>
+        [Required]
         public string CorpId { get; set; }
 
         /// <summary>
         /// AgentId
         /// </summary>
+        [Required]
+        [RegularExpression(@"[1-9]\d+", ErrorMessage = "请正确填写AgentId")]
         public long AgentId { get; set; }
 
         /// <summary>
         /// AppKey
         /// </summary>
+        [Required]
         public string AppKey { get; set; }
 
         /// <summary>
         /// AppSecret
         /// </summary>
+        [Required]
         public string AppSecret { get; set; }
 
         /// <summary>
         /// 加密 aes_key
         /// </summary>
+        [Required]
         public string Aes_Key { get; set; }
 
         /// <summary>
         /// 签名 token
         /// </summary>
+        [Required]
         public string Token { get; set; }
     }
 }
--- ContactsSync/VirtualLdap.Application/WorkWeixinUtils/WorkWeixinConfigOptions.cs
+++ ContactsSync/VirtualLdap.Application/WorkWeixinUtils/WorkWeixinConfigOptions.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace VirtualLdap.Application.WorkWeixinUtils;
 
 public class WorkWeixinConfigOptions
@@ -7,25 +9,31 @@ public class WorkWeixinConfigOptions
     /// <summary>
     /// CorpId
     /// </summary>
+    [Required]
     public string CorpId { get; set; }
 
     /// <summary>
     /// AgentId
     /// </summary>
+    [Required]
+    [RegularExpression(@"[1-9]\d+", ErrorMessage = "请正确填写AgentId")]
     public long AgentId { get; set; }
 
     /// <summary>
     /// AppSecret
     /// </summary>
+    [Required]
     public string AppSecret { get; set; }
 
     /// <summary>
     /// EncodingAESKey
     /// </summary>
+    [Required]
     public string EncodingAESKey { get; set; }
 
     /// <summary>
     /// token
     /// </summary>
+    [Required]
     public string Token { get; set; }
 }
