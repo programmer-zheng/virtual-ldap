45f2a72b351d6599a7727cb7c6ff0d307ccb7613
郑伟 <511927343@qq.com>
2023-12-03 12:06:18 +0800

    删除旧的项目

--- ContactsSync/VirtualLdap.Application/AppService/Dtos/ManageDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VirtualLdap.Application.AppService.Dtos
-{
-    public class ManageDto
-    {
-        public UserSimpleDto User { get; set; }
-
-        public List<DeptUserDto> UserList { get; set; }
-    }
-}
--- ContactsSync/VirtualLdap.Core/LdapConsts.cs
+++
@@ -1,6 +0,0 @@
-namespace VirtualLdap.Core;
-
-public class LdapConsts
-{
-    public const string CookieName = "Ldap_UserId";
-}
