25a4730f53633ab81b83f2d082d6fe6fb5b93463
郑伟 <511927343@qq.com>
2024-04-02 23:22:44 +0800

    完善企微与钉钉单元测试

---
+++ ContactsSync/test/ContactsSync.Application.Tests/Provider/DingDingProviderTests.cs
@@ -0,0 +1,50 @@
+﻿using ContactsSync.Application.Contracts.OpenPlatformProvider;
+using ContactsSync.Domain.Shared;
+using Newtonsoft.Json;
+using Shouldly;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ContactsSync.Application.Tests.Provider;
+
+public class DingDingProviderTests : ContactsSyncApplicationTestBase
+{
+    private readonly IOpenPlatformProviderApplicationService _openPlatformProviderApplicationService;
+
+    public readonly ITestOutputHelper Output;
+
+    public DingDingProviderTests(ITestOutputHelper output)
+    {
+        _openPlatformProviderApplicationService = GetRequiredKeyedService<IOpenPlatformProviderApplicationService>(OpenPlatformProviderEnum.DingDing.ToString());
+        Output = output;
+    }
+
+    [Fact]
+    public async Task TestGetAccessTokenAsync()
+    {
+        var token = await _openPlatformProviderApplicationService.GetAccessTokenAsync();
+        Output.WriteLine($"AccessToken：{token}");
+        Assert.NotEmpty(token);
+    }
+
+    [Fact]
+    public async Task TestGetDepartmentListAndUsersAsync()
+    {
+        var departments = await _openPlatformProviderApplicationService.GetDepartmentListAsync();
+        departments.ShouldNotBeNull().ShouldNotBeEmpty();
+        var department = departments.First();
+        var users = await _openPlatformProviderApplicationService.GetDeptUserListAsync(department.DepartmentId);
+        users.ShouldNotBeNull().ShouldNotBeEmpty();
+        Output.WriteLine("部门列表：");
+        Output.WriteLine(JsonConvert.SerializeObject(departments));
+        Output.WriteLine($"部门【{department.DeptName}】下人员信息：");
+        Output.WriteLine(JsonConvert.SerializeObject(users));
+    }
+
+    [Fact]
+    public async Task TestCreateApprovalTemplate()
+    {
+        var spNo = await _openPlatformProviderApplicationService.CreateApprovalTemplate();
+        spNo.ShouldNotBeNullOrEmpty();
+    }
+}
---
+++ ContactsSync/test/ContactsSync.Application.Tests/Provider/WeWorkProviderTests.cs
@@ -0,0 +1,50 @@
+﻿using ContactsSync.Application.Contracts.OpenPlatformProvider;
+using ContactsSync.Domain.Shared;
+using Newtonsoft.Json;
+using Shouldly;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ContactsSync.Application.Tests.Provider;
+
+public class WeWorkProviderTests : ContactsSyncApplicationTestBase
+{
+    private readonly IOpenPlatformProviderApplicationService _openPlatformProviderApplicationService;
+
+    public readonly ITestOutputHelper Output;
+
+    public WeWorkProviderTests(ITestOutputHelper output)
+    {
+        _openPlatformProviderApplicationService = GetRequiredKeyedService<IOpenPlatformProviderApplicationService>(OpenPlatformProviderEnum.WeWork.ToString());
+        Output = output;
+    }
+
+    [Fact]
+    public async Task TestGetAccessTokenAsync()
+    {
+        var token = await _openPlatformProviderApplicationService.GetAccessTokenAsync();
+        Output.WriteLine($"AccessToken：{token}");
+        Assert.NotEmpty(token);
+    }
+
+    [Fact]
+    public async Task TestGetDepartmentListAndUsersAsync()
+    {
+        var departments = await _openPlatformProviderApplicationService.GetDepartmentListAsync();
+        departments.ShouldNotBeNull().ShouldNotBeEmpty();
+        var department = departments.First();
+        var users = await _openPlatformProviderApplicationService.GetDeptUserListAsync(department.DepartmentId);
+        Assert.NotEmpty(users);
+        Output.WriteLine("部门列表：");
+        Output.WriteLine(JsonConvert.SerializeObject(departments));
+        Output.WriteLine($"部门【{department.DeptName}】下人员信息：");
+        Output.WriteLine(JsonConvert.SerializeObject(users));
+    }
+
+    [Fact]
+    public async Task TestCreateApprovalTemplate()
+    {
+        var spNo = await _openPlatformProviderApplicationService.CreateApprovalTemplate();
+        spNo.ShouldNotBeNullOrEmpty();
+    }
+}
